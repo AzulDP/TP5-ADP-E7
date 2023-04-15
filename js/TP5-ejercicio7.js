f549b491a0c921dc7d0844c71816fb959d5c3c42
Azul De Pol <azul.depol@gmail.com>
2023-04-15 17:54:53 +0100

    agregue el enunciado

---
+++ js/TP5-ejercicio7.js
@@ -0,0 +1,16 @@
+/*
+Piedra-papel-tijera 👊🏻 ✋🏻 ✌🏻 : 
+crear un script con toda la logica necesaria que solicite al usuario un movimiento (piedra - papel - tijera) la PC elija aleatoriamente un movimiento y al final mostrar por pantalla quien gano, luego preguntar si queremos jugar otra vez. Este ejercicio forma parte del TP4 (se puede realizar con los conceptos vistos hasta hoy)
+
+piedra 0 le gana a tijera 2
+piedra 0 pierde contra papel 1 == papel 1 gana contra piedra 0
+papel 1 pierde contra tijera 2 == tijera 2 gana contra papel 1
+piedra 0 = a piedra 0 empata
+papel 1 = papel 1 empata
+tijera 2 = tijera 2 empata
+
+// }else if (isNaN(true)) {
+//   alert("Ingrese un número");
+ */
+
+
