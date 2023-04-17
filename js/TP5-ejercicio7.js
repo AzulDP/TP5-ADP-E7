d070c30c120ba49fdc54d41d3a8d4d711451a7a7
Azul De Pol <azul.depol@gmail.com>
2023-04-17 23:14:24 +0100

    segui trabajando la logica d la tirada y los mensajes

--- js/TP5-ejercicio7.js
+++ js/TP5-ejercicio7.js
@@ -3,55 +3,48 @@ Piedra-papel-tijera 👊🏻 ✋🏻 ✌🏻 :
 crear un script con toda la logica necesaria que solicite al usuario un movimiento (piedra - papel - tijera) la PC elija aleatoriamente un movimiento y al final mostrar por pantalla quien gano, luego preguntar si queremos jugar otra vez. Este ejercicio forma parte del TP4 (se puede realizar con los conceptos vistos hasta hoy)
 */
 let userMove = [];
+let computerMove = [];
+let message = "";
 let formulario = document.querySelector("form");
-console.log(formulario);
+
 
 formulario.addEventListener("submit", obtenerDato);
 
-function obtenerDato(e){
+function obtenerDato(e) {
     e.preventDefault();
     console.log("Ejecutando el evento submit del formulario");
-    let input = [document.querySelector("input")];
-    console.log(typeof input);
-    console.log(input.value);
-    tirada();
+    let userMove = parseInt(document.getElementById("userMove").value);
+    console.log(`user move is ${userMove}`);
+    let computerMove = Math.floor(Math.random() * 3) + 1;
+    console.log(`computer move is ${computerMove}`);
+    tirada;
 }
 
-function tirada(){
-    for(tirada=0; tirada<6; tirada++){
-        // let userMove = obtenerDato(input.value);
-        
-let userMove = parseInt(prompt("Seleccione un valor del cero al 2 donde:  Piedra -> 0; Papel -> 1; Tijera  -> 2"));
-console.log(`user move is ${userMove}`);
-let computerMove = Math.floor(Math.random()*3)+1;
-console.log(`computer move is ${computerMove}`);
+function tirada() {
+    for (tirada = 0; tirada < 6; tirada++) {
+        if (userMove === 0 && computerMove === 0 ||
+            userMove === 1 && computerMove === 1 ||
+            userMove === 2 && computerMove === 2) {
+            document.getElementById("displayUserMove").innerHTML(`<p>Tu jugada ${userMove}</p>`)
+            document.getElementById("displayComputerMove").innerHTML(`<p>La jugada de la computadora ${computerMove}</p>`)
+            document.getElementById("displayResult").innerHTML(`<p>Empataron!</p>`)
+        } else if (userMove === 0 && computerMove === 1 ||
+            userMove === 1 && computerMove === 2 ||
+            userMove === 2 && computerMove === 0) {
+            document.getElementById("displayUserMove").innerHTML(`<p>Tu jugada ${userMove}</p>`)
+            document.getElementById("displayComputerMove").innerHTML(`<p>La jugada de la computadora ${computerMove}</p>`)
+            document.getElementById("displayResult").innerHTML(`<p>Perdiste! Mejor suerte la próxima vez!</p>`)
+        } else if (userMove === 0 && computerMove === 2 ||
+            userMove === 1 && computerMove === 0 ||
+            userMove === 2 && computerMove === 1) {
+            document.getElementById("displayUserMove").innerHTML(`<p>Tu jugada ${userMove}</p>`)
+            document.getElementById("displayComputerMove").innerHTML(`<p>La jugada de la computadora ${computerMove}</p>`)
+            document.getElementById("displayResult").innerHTML(`<p>Ganaste!</p>`)
+        }
+    }
+    // alert("¿Quieres jugar de nuevo?");
+}
 
-// if (userMove === 0 || userMove === 1 || userMove === 2){
-//     document.write(`${userMove}`);
-// } else if (userMove > 2 || userMove == NaN || userMove == ""){
-//     alert("El valor ingresado no es válido. Ingrese un número del 0 al 2, por favor.")
-// } 
-
-
-if (userMove === 0 && computerMove === 0 ||
-userMove === 1 && computerMove === 1 ||
-userMove === 2 && computerMove === 2 ) {
-    document.write(`<p>Empataron!</p>`)
-}
-if (userMove === 0 && computerMove === 1 ||
-userMove === 1 && computerMove === 2 ||
-userMove === 2 && computerMove === 0 ) {
-    document.write(`<p>Perdiste! Mejor suerte la próxima vez!</p>`)
-}
-if (userMove === 0 && computerMove === 2 ||
-userMove === 1 && computerMove === 0 ||
-userMove === 2 && computerMove === 1 ) {
-    document.write(`<p>Ganaste!</p>`)
-}
-}
-// alert("¿Quieres jugar de nuevo?");
-}
-    
 // }
 
 
